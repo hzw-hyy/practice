4f55b6b0920e594985d328709b0967ecc3ea6318
hyy <18845634144@163.com>
2020-07-15 13:24:45 +0800

    版本更新2

---
+++ moudle_1/src/test/java/com/example/hyy/practice/reflection/GitDemo.java
@@ -0,0 +1,14 @@
+package com.example.hyy.practice.reflection;
+
+/**
+ * @author hyy
+ * @version 1.0
+ * @date 2020/7/15 13:16
+ */
+public class GitDemo {
+
+    public static void main(String[] args) {
+        System.out.println("版本更新1");
+        System.out.println("版本更新2");
+    }
+}
